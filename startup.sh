1411e9422efcce700e680e02dd50e430ec91f43f
JackMthembu <hello@jackmthembu.com>
2025-03-26 17:48:09 +0200

    Fix: Switch from ODBC to MySQL client libraries

--- startup.sh
+++ startup.sh
@@ -6,11 +6,9 @@ set -e
 # Print commands
 set -x
 
-# Install ODBC driver
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+# Install MySQL client libraries
 apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+apt-get install -y default-libmysqlclient-dev build-essential pkg-config
 
 # Activate the virtual environment if it exists
 if [ -d "antenv" ]; then
