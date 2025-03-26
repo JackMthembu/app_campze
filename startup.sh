bfa876d1c47c4b0cfbb9766fee893f7ebd0e2a7b
JackMthembu <hello@jackmthembu.com>
2025-03-26 17:31:48 +0200

    Fix: Add ODBC driver and pyodbc package

--- startup.sh
+++ startup.sh
@@ -6,6 +6,12 @@ set -e
 # Print commands
 set -x
 
+# Install ODBC driver
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+
 # Activate the virtual environment if it exists
 if [ -d "antenv" ]; then
     source antenv/bin/activate
